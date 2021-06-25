3bd1a9444fd6a8be7ac85aa7f449149e04f478c1
prajwal-naik <naikprajwal40@gmail.com>
2021-06-25 13:22:45 +0530

    genres navbar

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
 
 export default function Home() {
   return (
@@ -17,7 +18,9 @@ export default function Home() {
       
       {/*Navbar*/}
       <Nav />
+
       {/*Results*/}
+      <Results/>
     </div>
   )
 }
